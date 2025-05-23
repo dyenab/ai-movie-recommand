cdadb81ebe709b14e60b4e1d5346f7e7ebef3eea
예나 <dyenab@MacBook-Pro.local>
2025-05-24 02:12:34 +0900

    push

--- serverless/api/recommend.js
+++ serverless/api/recommend.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 }
 
 async function get3Movies(genres) {
-  consoke.log("장르:", genres);
+  console.log("장르:", genres);
   const movies = [];
   const seen = new Set();
   let retry = 0;
